0d25d972d57b871d6b4f3c0bbb582d5aa2102836
wheslancardoso <wheslancardoso1@gmail.com>
2026-06-11 11:27:43 -0300

    chore: atualiza credenciais do Supabase no app_constants e adiciona .env no gitignore

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,8 +1,8 @@
 class AppConstants {
   AppConstants._();
 
-  static const String supabaseUrl = 'https://wddebrieixjcxurtggmb.supabase.co';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkZGVicmllaXhqY3h1cnRnZ21iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzNjAzOTQsImV4cCI6MjA4NDkzNjM5NH0.Sd5kIKX0iZNaFNL2d6s2TMRQcxljw9s2V2KN-dcAykA';
+  static const String supabaseUrl = 'https://vgsjpuxymtkkiaissrky.supabase.co';
+  static const String supabaseAnonKey = 'sb_publishable_6D8ptLACddu7D5r2SN0LTQ_RQMtS15q';
   static const String baseApiUrl = 'http://localhost:3000';
 
   static const Duration defaultTimeout = Duration(seconds: 30);
